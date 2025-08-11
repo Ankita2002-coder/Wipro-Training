34b9037b62d517dd1cc05db02b01b19b7962a811
Ankita Rayaguru <ankitarayaguru@gmail.com>
2025-08-12 00:05:39 +0530

    Create w7js.java

---
+++ Day2/PKG/w7js.java
@@ -0,0 +1,12 @@
+public class CarImpl {
+	package PKG;
+}
+ 
+	public static void main(String[] args) {
+		Car car = new Car("Mercedez",2025);
+		System.out.println(car);
+		Car newcar = new Car("BMW",2025);
+		System.out.println(newcar);
+		Car democar = new Car();
+	}
+ 
